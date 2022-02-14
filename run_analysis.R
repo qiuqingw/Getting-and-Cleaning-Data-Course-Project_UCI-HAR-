4c70c8fd6b4be5c1a08e871646e0f57f9e36adcf
qiuqingw <wangq@ccf.org>
2022-02-14 14:09:36 -0500

    Initial Requirements for run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,14 @@
+## Getting and Cleaning Data Course Project using UCI HAR Datasets.
+
+## Read the train and test datasets about Human activity recognition using smartphones.
+
+## Merge the train and test datasets together.
+
+## Extract the measurements on the mean and standard deviation for each measurement.
+
+## Name the activities variables in the dataset.
+
+## Label the data set with descriptive variable names.
+
+## Create a second, independent tidy data set 
+## with the average of each variable for each activity and each subject.
